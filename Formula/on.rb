7d89f5c3c9d0b0a43afb22a7a2b4631dcea787fe
rihuachen93 <rickyshin@foxmail.com>
2026-04-14 21:19:33 +0800

    Update on to v0.2.1

--- Formula/on.rb
+++ Formula/on.rb
@@ -1,13 +1,13 @@
 class On < Formula
   desc "A CLI tool to launch your dev environment with one command"
   homepage "https://github.com/rickyshin93/launch"
-  version "0.2.0"
+  version "0.2.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/rickyshin93/launch/releases/download/v0.2.0/on-v0.2.0-aarch64-apple-darwin.tar.gz"
-      sha256 "b190fd7f66b4c6dfae1867017d5520e2b56c49c0f8bb9f714387fcd3bd3cbb52"
+      url "https://github.com/rickyshin93/launch/releases/download/v0.2.1/on-v0.2.1-aarch64-apple-darwin.tar.gz"
+      sha256 "22b36f5e5d6bd8fa444dc4a5a1e7e2260007ce1b94da737aac0ed46daba13feb"
     end
   end
 
