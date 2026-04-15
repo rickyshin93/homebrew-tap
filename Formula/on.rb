8ea54f74475b40c7fe27f0a75feeb4be83d89fef
rickyshin93 <rickyshin@foxmail.com>
2026-04-15 13:41:46 +0000

    chore: bump on to v0.4.0

--- Formula/on.rb
+++ Formula/on.rb
@@ -1,20 +1,20 @@
 class On < Formula
   desc "A CLI tool to launch your dev environment with one command"
-  homepage "https://github.com/rickyshin93/launch"
-  version "0.3.3"
+  homepage "https://github.com/rickyshin93/on"
+  version "0.4.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/rickyshin93/launch/releases/download/v0.3.3/on-v0.3.3-aarch64-apple-darwin.tar.gz"
-      sha256 "cb49d29d2433bc78f63fb97a8465b336df2737b807f3876261675dfcb9b14a35"
+      url "https://github.com/rickyshin93/on/releases/download/v0.4.0/on-v0.4.0-aarch64-apple-darwin.tar.gz"
+      sha256 "c53a74abe44513f8f379cff5c6a45e461e0ed6293f690a699bdf1e6a9c6c87a7"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/rickyshin93/launch/releases/download/v0.3.3/on-v0.3.3-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "b420a0e3f7c43a679a8452107facd2481ccb39d75c5902a30469547c25289b2c"
+      url "https://github.com/rickyshin93/on/releases/download/v0.4.0/on-v0.4.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "18de34f670a649ba944698159b71a90d30ba49f191e7cb8a9839d1fb70069eed"
     end
   end
 
